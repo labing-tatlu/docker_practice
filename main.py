dff0d085e0504e1d82903d67531d45f209698401
labing-tatlu <labing.tatlu@gmail.com>
2023-01-09 12:15:11 +0100

    Creating apps and dockerfile/docker-compose

---
+++ main.py
@@ -0,0 +1,10 @@
+import calendar
+
+print('Welcome to the calendar\n')
+
+year = int(input('Input year: '))
+month = int(input('Input month number: '))
+
+print(calendar.month(year, month))
+
+print('Good luck! :)')
